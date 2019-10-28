f1d97ca303c002bb4c95067f956eca5d9837bc56
Bibrak <bibrakc@gmail.com>
2019-10-28 12:53:43 -0400

    openmp parallelized

--- VectorMatrices/main.c
+++ VectorMatrices/main.c
@@ -1,8 +1,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <omp.h>
 //Mat Mat Multiply
+
+//sudo perf stat -e task-clock,cycles,instructions,cache-references,cache-misses
 
 #define N 2000
 
@@ -11,60 +13,56 @@ int main()
 {
 
   int r1, c1, r2, c2, c, d, k, sum = 0;
-  
-  int **first = malloc(N*sizeof(int*));
-  
-  int **second = malloc(N*sizeof(int*));
 
-  int **multiply = malloc(N*sizeof(int*));
+  int *first = malloc(N*N*sizeof(int));
 
-  
- 
-  for(int i=0;i< N;i++){
+  int *second = malloc(N*N*sizeof(int));
 
-    first[i] = malloc(N*sizeof(int));
-    second[i] = malloc(N*sizeof(int));
-    multiply[i] = malloc(N*sizeof(int));
-
-  }
-  
-
-  
+  int *multiply = malloc(N*N*sizeof(int));
 
 
   printf("Initializing Matrices\n");
 
+
+  #pragma omp parallel shared(first, second, multiply) private(c,d,k) 
+  {
+printf("Thread rank: %d\n", omp_get_thread_num());
+  #pragma omp for
   for (c = 0; c < N; c++)
     for (d = 0; d < N; d++)
-      first[c][d] = 1;
+      first[c*N +d] = 1;
       //scanf("%d", &first[c][d]);
+  #pragma omp  for
+  for (c = 0; c < N; c++)
+    for (d = 0; d < N; d++)
+      second[c*N +d] = 2;
 
+  #pragma omp for
+  for (c = 0; c < N; c++) {
 
-
-    for (c = 0; c < N; c++)
-      for (d = 0; d < N; d++)
-          second[c][d] = 2;
-
-    for (c = 0; c < N; c++) {
-      for (d = 0; d < N; d++) {
-        for (k = 0; k < N; k++) {
-          sum = sum + first[c][k]*second[k][d];
-        }
-
-        multiply[c][d] = sum;
-        sum = 0;
+    for (d = 0; d < N; d++) {
+      multiply[c*N+d] = 0;
+      //sum = 0;
+      for (k = 0; k < N; k++) {
+        multiply[c*N+d] = multiply[c*N+d] + first[c*N+k]*second[k*N+d];
       }
+
+      //multiply[c*N+d] = sum;
+
     }
+  }
+
+}
 /*
     printf("Product of the matrices:\n");
 
     for (c = 0; c < N; c++) {
       for (d = 0; d < N; d++)
-        printf("%d\t", multiply[c][d]);
+        printf("%d\t", multiply[c*N+d]);
 
       printf("\n");
     }
- */
+*/
 
 
 //free the memory
